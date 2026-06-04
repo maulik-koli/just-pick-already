8782232c10fdd68a13d991c34743fb26108b253b
Maulik Koli <maulikkoli1021@gmail.com>
2026-06-04 12:54:12 +0530

    add fav icon

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,6 +4,7 @@ import "dotenv/config";
 const envSchema = z.object({
     NODE_ENV: z.enum(['development', 'production']),
     DATABASE_URL: z.url("Invalid DATABASE_URL"),
+    DIRECT_URL: z.url("Invalid DIRECT_URL"),
     GEMINI_MODEL_CODE: z.string().min(1, "Gemini model code required"),
     GEMINI_API_KEY: z.string().min(1, "Gemini api key required"),
 });
@@ -11,6 +12,7 @@ const envSchema = z.object({
 export const env = envSchema.parse({
     NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
+    DIRECT_URL: process.env.DIRECT_URL,
     GEMINI_MODEL_CODE: process.env.GEMINI_MODEL_CODE,
     GEMINI_API_KEY: process.env.GEMINI_API_KEY,
 });
